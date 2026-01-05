eccfe1ab80a04d34e915cac3f8b0806964ac0c65
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2026-01-05 23:58:44 +0500

    Add initial HTML structure and product1 object in app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+let product1={
+    nam:"Shoe",
+    price:1999,
+    reviews:[
+        {name:"Azam",Rating:"5Stars",text:"Kya joote hai"},
+        {name:"Abdullah",Rating:"2Stars",text:"Farig Joote hai"},
+        {name:"Bilal",Rating:"3Stars",text:"Haramiiiiiiiii"}
+    ]
+}
+console.log(product1);
