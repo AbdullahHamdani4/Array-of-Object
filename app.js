ee690ba29306e209d6cd240f80e03d2ed3f2f7a3
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2026-01-06 10:55:39 +0500

    Restore product1 object and constructor function in app.js

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-// let product1={
-//     nam:"Shoe",
-//     price:1999,
-//     reviews:[
-//         {name:"Azam",Rating:"5Stars",text:"Kya joote hai"},
-//         {name:"Abdullah",Rating:"2Stars",text:"Farig Joote hai"},
-//         {name:"Bilal",Rating:"3Stars",text:"Haramiiiiiiiii"}
-//     ]
-// }
-// console.log(product1);
-// function constructor(nam,price,review1Name,review1Rating,review2Name,review2Rating) {
-//     this.name=nam
-//     this.price=price
-//     this.review=[
-//         {nam:review1Name,rating:review1Rating},
-//         {nam:review2Name,rating:review2Rating}
-//     ]
-// }
-// let ob1=new constructor("Abdullah",1999,"hasna",5,"biala",4 )
-// console.log(ob1);
+let product1={
+    nam:"Shoe",
+    price:1999,
+    reviews:[
+        {name:"Azam",Rating:"5Stars",text:"Kya joote hai"},
+        {name:"Abdullah",Rating:"2Stars",text:"Farig Joote hai"},
+        {name:"Bilal",Rating:"3Stars",text:"Haramiiiiiiiii"}
+    ]
+}
+console.log(product1);
+function constructor(nam,price,review1Name,review1Rating,review2Name,review2Rating) {
+    this.name=nam
+    this.price=price
+    this.review=[
+        {nam:review1Name,rating:review1Rating},
+        {nam:review2Name,rating:review2Rating}
+    ]
+}
+let ob1=new constructor("Abdullah",1999,"hasna",5,"biala",4 )
+console.log(ob1);
