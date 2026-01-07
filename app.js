a9a458fcc023db507238a07425ecfc0786c1b526
AbdullahHamdani4 <abdullah.hamdani87@gmail.com>
2026-01-07 15:33:48 +0500

    object created

--- app.js
+++ app.js
@@ -16,5 +16,20 @@ function Constructor(nam, price, review1Name, review1Rating, review2Name, review
         { nam: review2Name, rating: review2Rating }
     ]
 }
+let feebacks = [
+    { name: "babar azam", description: "Behtareen joote", rating: "5star" },
+    { name: "Safee azam", description: "Alaa joote", rating: "3star" },
+    { name: "Abdul azam", description: "Harami joote", rating: "1star" }
+]
+function showReviews(params) {
+    reviews.classList.toggle("hide")
+    reviews.innerHTML = `
+     <ul>
+                    <li>${feebacks[0].name}:${feebacks[0].description}</li>
+                    <li>${feebacks[1].name}:${feebacks[1].description}</li>
+                    <li>${feebacks[2].name}:${feebacks[2].description}</li>
+                 </ul>
+                `
+}
 // let ob1 = new constructor("Abdullah", 1999, "hasna", 5, "biala", 4)
 // console.log(ob1);
